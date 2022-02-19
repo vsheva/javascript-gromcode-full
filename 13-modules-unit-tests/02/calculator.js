9e0631b85607c58e759cb3a18129c961c48d6868
vsheva <valera.sheva@gmail.com>
2022-02-19 22:22:55 +0200

    fix

--- 13-modules-unit-tests/02/calculator.js
+++ 13-modules-unit-tests/02/calculator.js
@@ -1,4 +1,4 @@
 
 export const getSquaredNumbers = (array) => array.filter(item => item * item);
-export const getOddNumbers = (array) => array.filter(item => item % 2);
+export const getOddNumbers = (array) => array.filter(item => item % 2===1);
 export default (a, b) => a + b;
--- 13-modules-unit-tests/02/calculator.test.js
+++ 13-modules-unit-tests/02/calculator.test.js
@@ -1,7 +1,17 @@
-import {getSquaredNumbers,getOddNumbers} from "./calculator"
+import getSum, {getSquaredNumbers,getOddNumbers} from "./calculator"
 
 it("should get squared numbers", ()=>{
     const result =getSquaredNumbers([1,2,3]);
     expect(result).toEqual([1,4,9]);
 });
 
+
+it("should keep  odd numbers only", ()=>{
+    const result =getOddNumbers([1,2,3,4,5]);
+    expect(result).toEqual([1,4,5]);
+});
+
+it("should get sum of numbers", ()=>{
+    const result =getSum([8,4]);
+    expect(result).toEqual([12]);
+});
