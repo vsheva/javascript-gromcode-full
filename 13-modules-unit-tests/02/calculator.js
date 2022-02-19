f093c9c3cb19caaeeed3da72575843562c849016
vsheva <valera.sheva@gmail.com>
2022-02-19 22:39:48 +0200

    fix

--- 13-modules-unit-tests/02/calculator.js
+++ 13-modules-unit-tests/02/calculator.js
@@ -1,10 +1,3 @@
 export const getSquaredArray = (array) => array.map(item => item * item);
 export const getOddNumbers = (array) => array.filter(item => item % 2 === 1);
 export default (a, b) => a + b;
-
-//
-// export const getSquaredArray = arr => arr.map(el => el * el)
-//
-// export const getOddNumbers = arr => arr.filter(el => el%2 !== 0)
-//
-// export default (num1, num2) => num1 + num2
