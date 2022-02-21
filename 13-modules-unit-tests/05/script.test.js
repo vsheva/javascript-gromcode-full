662421cd0fc931b3fac9bc7c793bc68a177c0c02
vsheva <valera.sheva@gmail.com>
2022-02-21 09:20:39 +0200

    fix

--- 13-modules-unit-tests/05/script.test.js
+++ 13-modules-unit-tests/05/script.test.js
@@ -2,7 +2,7 @@ import  {reverseArray} from "./script.js";
 
 
 it("should get reversed array", ()=>{
-    expect(reverseArray([1, 5, 7, 11, 5])).toEqual([5, 11, 7, 5, 11]);
+    expect(reverseArray([1, 5, 7, 11, 5])).toEqual([5, 11, 7, 5, 1]);
 });
 
 it("should get null if not array", ()=>{
--- 13-modules-unit-tests/06/script.js
+++ 13-modules-unit-tests/06/script.js
@@ -1,18 +1,8 @@
-//algo
-//1. find Math.abs()
-//2. find Max
-//3.Square result
+const withdraw = (clients, balances, client, amount) => {
 
-export let getMinSquaredNumber =(array)=>{
-   if(!Array.isArray(array) || array.length===0) {
-       return null;
-   }
-    let newArr = array.map(item=>Math.abs(item))
-    let max = Math.min(...newArr);
-    return max*max;
-
-
+        const balanceClient = balances[clients.indexOf(client)]
+    if (balanceClient > amount) {
+        return balanceClient - amount
+    }
+    return -1
 }
-
-console.log(getMinSquaredNumber([2,-8,5,4]))
-
