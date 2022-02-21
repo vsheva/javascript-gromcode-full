d80debd0343566f962814ab9b846d90fc2f627f5
vsheva <valera.sheva@gmail.com>
2022-02-21 08:54:43 +0200

    fix

--- 13-modules-unit-tests/05/script.test.js
+++ 13-modules-unit-tests/05/script.test.js
@@ -1,7 +1,14 @@
 import  {reverseArray} from "./script.js";
 
+
 it("should get reversed array", ()=>{
     expect(reverseArray([1, 5, 7, 11, 5])).toEqual([5, 11, 7, 5, 11]);
 });
 
+it("should get null if not array", ()=>{
+    expect(reverseArray("Hello world")).toEqual(null);
+});
 
+it("should get null if array is empty", ()=>{
+    expect(reverseArray([])).toEqual(null);
+});
