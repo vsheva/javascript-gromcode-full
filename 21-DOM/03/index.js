88d3f765af8fdcea6bce66c18f4b0ca0433cefd0
vsheva <valera.sheva@gmail.com>
2022-03-21 21:49:40 +0200

    fix

---
+++ 21-DOM/03/index.js
@@ -0,0 +1,13 @@
+export function getItemsList() {
+    const elementsList = document.querySelectorAll('.technology')
+    console.dir(elementsList)
+}
+
+ export function getItemsArray() {
+    const nodeList = Array.from(document.querySelectorAll('.tool'))
+    console.dir(nodeList)
+    return nodeList
+}
+
+getItemsList()
+getItemsArray()
