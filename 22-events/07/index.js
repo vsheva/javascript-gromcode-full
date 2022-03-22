1b259473846e62eea03f073030b4f8df8cf01123
vsheva <valera.sheva@gmail.com>
2022-03-22 20:44:22 +0200

    fix

--- 22-events/07/index.js
+++ 22-events/07/index.js
@@ -1,8 +1,16 @@
-export function clearList() {
-    let category =document.querySelector(".categories")
-   category.innerHTML = ''
+//algo
+//quary all
+//handleClick => textContent
+// перебор: add eventListener to each elem
+
+const btns = document.querySelectorAll(".btn")
+
+const handleClick = (e) => {
+    console.log(e.target.textContent)
 }
 
-
+btns.forEach(element => {
+    element.addEventListener("click", handleClick)
+})
 
 
