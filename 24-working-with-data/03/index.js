f06ad1e8d55ffd80fb0c3ecccf2949bdfe413098
vsheva <valera.sheva@gmail.com>
2022-03-25 01:25:37 +0200

    fix

--- 24-working-with-data/03/index.js
+++ 24-working-with-data/03/index.js
@@ -1,14 +1,67 @@
-export function getItemsList() {
-    const elementsList = document.querySelectorAll('.technology')
-    console.dir(elementsList)
-    return elementsList
-}
+export const getDiff = (startDate, endDate) => {
+    const start = new Date(startDate).getTime()
+    const end = new Date(endDate).getTime()
+    const timeRemaining = Math.abs(end - start)/1000;
 
- export function getItemsArray() {
-    const nodeList = Array.from(document.querySelectorAll('.tool'))
-    console.dir(nodeList)
-    return nodeList
-}
 
-getItemsList()
-getItemsArray()
+    let days = Math.floor(timeRemaining / (24*60*60))
+    let hours = Math.floor((timeRemaining / 3600) %24)
+    let minutes = Math.floor((timeRemaining / 60) % 60)
+    let seconds = Math.floor(timeRemaining % 60);
+
+    return  `${days}d ${hours}h ${minutes}m ${seconds}s`
+
+ }
+
+
+const firstDate = new Date(2022, 2, 24, 5, 10, 2);
+const secondDate = new Date(2022, 2, 25, 6, 15, 5);
+console.log(getDiff(secondDate, firstDate))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const getDiff = (startDate, endDate) => {
+//
+//     const date1 = new Date(startDate).getTime()
+//     console.log(date1)
+//     const date2 = new Date(endDate).getTime()
+//
+//     return new Date(date2 - date1)
+// }
+// getDiff(10, 5)
+//
+//
+// console.log(new Date(1990, 0, 1).getTime())
