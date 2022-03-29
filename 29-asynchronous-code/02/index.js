8c308bf10fb8eaaedb1dff90f7cc98eba4f98977
vsheva <valera.sheva@gmail.com>
2022-03-30 00:49:10 +0300

    fix

--- 29-asynchronous-code/02/index.js
+++ 29-asynchronous-code/02/index.js
@@ -1,89 +1,13 @@
-const generateNumbersRange = (from, to) => {
-    const result = [];
+const pinger = (count, period) => {
+    let i= count;
+    console.log("Ping");
+   const interval = setInterval(()=>{
+       if (--i>0) {
+           console.log("Ping");
+       } else {
+           clearInterval(interval);
+       }
+    }, period);
+}
 
-    for (let i = from; i <= to; i += 1) {
-        result.push(i);
-    }
-    return result;
-};
-
-
-//1. линия сидений
-const getLineSets = () =>
-    generateNumbersRange(1, 10)
-        .map(seatNumber => `
-         <div
-           class="sector__seat"
-           data-seat-number="${seatNumber}"
-         ></div>`)
-        .join('');
-
-
-//2. генерируем строки внутри сектора
-const getSectorLines = () => {
-    //высчитывает 1 раз, а не 10 и помещаем в ${seatsStirng} (замена getSectorLines)
-    const seatsStirng = getLineSets();
-
-    //вставили generateNumbersRange,генерируем линии
-    return generateNumbersRange(1, 10)
-        .map(lineNumber => `
-         <div
-           class="sector__line"
-           data-line-number="${lineNumber}"
-         >${seatsStirng}</div>`)
-        //линию заполняем сидениями (см.выше) и склеиваем в строку, после чего строка
-        //в ${linesString} ниже
-        .join('');
-};
-
-
-//генерируем 3 сектора
-//join и вставим в элемент arena
-const arenaElem = document.querySelector('.arena');
-
-const renderArena = () => {
-    const linesString = getSectorLines();
-
-    const sectorsString = generateNumbersRange(1, 3)
-        .map(sectorNumber => `
-         <div
-           class="sector"
-           data-sector-number="${sectorNumber}"
-         >${linesString}</div>`)
-        .join('');
-
-    arenaElem.innerHTML = sectorsString;
-};
-
-
-//делигирование
-const onSeatSelect = event => {
-    //проверить, сидение или нет т.е. класс ".sector__seat":  метод cclassList.contains('sector__seat')
-    const isSeat = event.target.classList.contains('sector__seat');
-    //если несидение - выхидим
-    if (!isSeat) {
-        return;
-    }
-
-    //cбор всей информации по расположению сидения
-    // data-sector-number => dataset.sectorNumber
-    //элемент, по к-ому кликнули - берем ближайший элемент (родитель) с классом .sectror - это нужный сектор
-    const sectorNumberElem = event.target.closest('.sector').dataset.sectorNumber;
-    const seatNumberElem = event.target.dataset.seatNumber;
-    //линия - это родительский элемент для сидения =чтобы получить ее, метод closest(это линиия, в которой сидение)
-    //dataset.lineNumber
-    const lineNumberElem = event.target.closest('.sector__line').dataset.lineNumber;
-
-
-    //выбираем элемент для вставки данных
-    const selectedSeatElem = document.querySelector('.board__selected-seat');
-    //вставляем текст через textContent (текст выводится как есть), append, prepend,innerHTML (если сами)
-    selectedSeatElem.textContent = `S ${sectorNumberElem} - L ${lineNumberElem} - S ${seatNumberElem}`;
-};
-
-//обработчик
-//const arenaElem = document.querySelector('.arena');
-arenaElem.addEventListener('click', onSeatSelect);
-
-
-renderArena();
+pinger(5, 1000)
--- 29-asynchronous-code/03/index.js
+++ 29-asynchronous-code/03/index.js
@@ -1,140 +1,19 @@
-//algo (with ID)
-//-add id to object
-//-tasksList.map(({text, done, id})
-//-checkbox.setAttribute('data-id', id);
+export const printing = () => {
+    console.log(1);
 
-const listElem = document.querySelector('.list');
-const createBtnElem = document.querySelector('.create-task-btn');
-const input = document.querySelector('.task-input');
+    setTimeout(function () {
+        console.log(5);
+    }, 1000);
 
-const tasks = [
-  { text: 'Buy milk', done: false, id: 1 },
-  { text: 'Pick up Tom from airport', done: false, id: 2 },
-  { text: 'Visit party', done: false, id: 3 },
-  { text: 'Visit doctor', done: true, id: 4 },
-  { text: 'Buy meat', done: true, id: 5 },
-];
+    console.log(2);
 
-const renderTasks = tasksList => {
-  listElem.innerHTML = '';
+    setTimeout(function () {
+        console.log(4);
+    }, 0);
 
-  const tasksElems = tasksList
-    .sort((a, b) => a.done - b.done)
-    .map(({ text, done, id }) => {
-      const listItemElem = document.createElement('li');
-      listItemElem.classList.add('list__item');
+    setTimeout(function () {
+        console.log(6);
+    }, 2000);
 
-      const checkbox = document.createElement('input');
-      checkbox.setAttribute('type', 'checkbox');
-      checkbox.setAttribute('data-id', id);
-      checkbox.checked = done;
-      checkbox.classList.add('list__item-checkbox');
-
-      if (done) {
-        listItemElem.classList.add('list__item_done');
-      }
-      listItemElem.append(checkbox, text);
-
-      return listItemElem;
-    });
-
-  listElem.append(...tasksElems);
+    console.log(3);
 };
-
-renderTasks(tasks);
-
-//algo
-//1. addTask
-//- addEventListener("click", handler)
-//handler:
-// ---- (if false=> return)
-// ---- create newTask object (text: inputElem.value, done:false; id:)
-// ---- tasks.push  task
-// ---- clear inputElem.value
-// ---- renderTasks(tasks)
-
-//2. toggleTask(event)
-
-// - addEventListener('click', toggleTask);
-//handler:
-//---tasks.forEach((task) => if (task.id === +event.target.dataset.id) {
-//--- if (task.done===false) task.done=true and reverse
-//---renderTasks(tasks)
-
-const addTask = () => {
-  if (!input.value) {
-    return;
-  }
-  const newTask = {
-    text: input.value,
-    done: false,
-    //date.stamp (ms from 01.01.1970)
-    //new Date(1900, 2).getTime() //-2221432423542
-    //getFullYear() getDate() ..день месяца getDay() .. номер дня недели
-    //setMonth(0,11)
-    //setDay(0,6)
-    id: Date.now(),
-  };
-  tasks.push(newTask);
-  input.value = '';
-  renderTasks(tasks);
-};
-
-createBtnElem.addEventListener('click', addTask);
-
-const toggleTask = event => {
-  tasks.forEach(task => {
-    if (task.id === +event.target.dataset.id) {
-      if (task.done === false) {
-        task.done = true;
-      } else {
-        task.done = false;
-      }
-    }
-  });
-
-  renderTasks(tasks);
-};
-
-listElem.addEventListener('click', toggleTask);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const renderTasks = listItems => {
-//     const list = document.querySelector(".list")
-//
-//     const listItemElems = listItems.map(itemText=> {
-//         const liElem= document.createElement("li")
-//         liElem.classList.add("list__item")
-//         liElem.append(itemText)
-//
-//         return  liElem
-//     })
-//
-//     list.append(...listItemElems)
-// }
-//
-// renderTasks(tasks)
