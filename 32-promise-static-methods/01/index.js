34fa80fe08b51edf79167d2a6daa9b8f72cd7c8e
vsheva <valera.sheva@gmail.com>
2022-04-01 16:50:39 +0300

    fix

--- 32-promise-static-methods/01/index.js
+++ 32-promise-static-methods/01/index.js
@@ -1,36 +1,45 @@
-'use strict';
+'use strict'
+
+//asyncSum(asyncNum1,asyncNum2, asyncNum3)
 
 // algo
-//new Promise()
-//  if (userId === 'broken') -> reject(new Error)
-// else -> resolve {}
+//1.etValueWithDelay(value, delay) => Promise -> resolve
+//2. asyncSum = (...asyncNumbers)=> PromiseAll().then(()=>getSum)
+//2-a.getSum(numbers) => numbers.filter().reduce()
 
-const requestUserData = userId => {
-  const promise = new Promise((resolve, reject) => {
-    if (userId === 'broken') {
-      setTimeout(() => {
-        reject(new Error('User not found'));
-      }, 500);
-    } else {
-      setTimeout(() => {
-        resolve({
-          name: 'John',
-          age: 17,
-          userId,
-          email: `${userId}@example.com`,
-        });
-      }, 1000);
-    }
-  });
+//3. asyncSum(numbers) => asyncSum().resolve.reject .then
 
-  return promise;
-};
 
-requestUserData('broken')
-    .catch(error => {
-        console.log(error);
-        throw new Error("Error");
-    })
-   .then(data => console.log(data))
-    .catch(error => console.log(error)
-  .finally(() => console.log('Hello')))
+//1.
+const getValueWithDelay = (value, delay) => new Promise(resolve => {
+    setTimeout(() => {
+        console.log(value)
+        resolve(value);
+    }, delay);
+});
+
+const asyncNum1 = getValueWithDelay("10", 1000)
+const asyncNum2 = getValueWithDelay(54, 2000)
+const asyncNum3 = getValueWithDelay("undef", 2000)
+
+
+const getSum = (numbers) => {
+    return numbers
+        .filter(val => !isNaN(val))
+        .reduce((acc, num) => acc + +num, 0)
+}
+
+//2.
+export const asyncSum = (...asyncNumbers) => {
+    return Promise.all(asyncNumbers)
+        .then(numbers => getSum(numbers))
+        .catch(() =>new Error("Can't calculate\\")); // !
+}
+
+//3.
+asyncSum(asyncNum1, Promise.reject(new Error("error")), asyncNum3)
+    .then(result => console.log(result))
+
+
+
+
--- 32-promise-static-methods/02/index.js
+++ 32-promise-static-methods/02/index.js
@@ -1,34 +1,52 @@
 //algo
-//  asyncCalculator(num)
-//  new Promise(resolve, reject)
-// .then
-//.then
-//.then
+//1.request (url)=>Promise
 
- const asyncCalculator = (numbr) =>
+//2.getUserASAP(userId) =>  return Promise.race
+// --userUrls=  servers.map =>
+//-- requests=userUrls.map=> request()
+//----return Promice.race(requests)
+
+
+const getRandomNumbers = (from, to) => from + Math.random() * (to - from);
+
+const request = (url) =>
     new Promise((resolve) => {
+    const delay = getRandomNumbers(1000, 3000);
+
     setTimeout(() => {
-        console.log(`Initial value: ${numbr}`);
+        resolve({
+            userData: {
+                name: "Tom",
+                age: 17,
+            },
+            source: url,
+        });
+    }, delay)
+});
 
-        resolve(numbr);
-    }, 500);
-})
-    .then(
-        (data) =>
-        new Promise((resolve) => {
-            setTimeout(() => {
-                let squared = data * data;
-                console.log(`Squared value: ${squared}`);
-                resolve(squared);
-            }, 500);
-        }))
-     .then((datas) =>{
-         let doubled = datas * 2;
-         console.log(`Doubled value: ${doubled}`)
-         return doubled;
-     });
 
-const result = asyncCalculator(5)
+const servers = [
+    "https://server.com/us",
+    "https://server.com/eu",
+    "https://server.com/au",
+]
+
+
+const getUserASAP = (userId) => {
+    const userUrls = servers.map((server) => `${server}/${userId}`);
+    //request(userUrl) --- создаем конкретный запрос
+    const requests = userUrls.map((userUrl) => request(userUrl))
+
+    return Promise.race(requests);
+}
+
+getUserASAP("user-id-1")
+    .then(data => console.log(data));
+
+
+
+
+
 
 
 
