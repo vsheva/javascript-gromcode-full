41336b066f6c310e2fe2fd6b6af45f601a7b291f
vsheva <valera.sheva@gmail.com>
2022-04-03 22:29:16 +0300

    fix

--- 33-http/01/index.js
+++ 33-http/01/index.js
@@ -1,28 +1,138 @@
-const userAvatarElem = document.querySelector('.user__avatar');
-const userNameElem = document.querySelector('.user__name');
-const userLocationElem = document.querySelector('.user__location');
+// algo
+//get all DOM elements
+// listener to buttonElem
+// http request:  fetchUserData(userName) => return fetch.then(response =>repsonse.json()
+
+// fetchUserData(userName).then(renderUserData(userData))
+
+//renderUserData(userData) => to each element
+
+const userAvatarElem = document.querySelector(".user__avatar");
+const userNameElem = document.querySelector(".user__name");
+const userLocationElem = document.querySelector(".user__location");
+const inputFormElem = document.querySelector(".name-form__input");
+const buttonElem = document.querySelector(".name-form__btn");
 
 const defaultAvatar = 'https://avatars3.githubusercontent.com/u1001';
-userAvatarElem.src = defaultAvatar;
+userAvatarElem.src = defaultAvatar
 
-const fetchUserData = userName =>
-    fetch(`https://api.github.com/users/${userName}`).then(response => response.json());
+//http request
+const fetchUserData = (userName) => {
+    return fetch(`https://api.github.com/users/${userName}`)
+        .then(response => response.json())
+}
 
-const renderUserData = userData => {
-    const { avatar_url, name, location } = userData;
+const renderUserData = (userData) => {
+    console.log(userData)
+    const {avatar_url, name, location} = userData
     userAvatarElem.src = avatar_url;
     userNameElem.textContent = name;
-    userLocationElem.textContent = location ? `from ${location}` : '';
-};
-
-const showUserBtnElem = document.querySelector('.name-form__btn');
-const userNameInputElem = document.querySelector('.name-form__input');
+    userLocationElem.textContent = location ? `from ${location}` : "";
+}
 
 const onSearchUser = () => {
-    const userName = userNameInputElem.value;
-    fetchUserData(userName).then(userData => renderUserData(userData));
-};
+    const userName = inputFormElem.value
+    fetchUserData(userName)
+        .then(userData => renderUserData(userData))
+}
 
-showUserBtnElem.addEventListener('click', onSearchUser);
+buttonElem.addEventListener("click", onSearchUser)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const userAvatarElem = document.querySelector('.user__avatar');
+// const userNameElem = document.querySelector('.user__name');
+// const userLocationElem = document.querySelector('.user__location');
+// const showUserBtnElem = document.querySelector('.name-form__btn');
+// const userNameInputElem = document.querySelector('.name-form__input');
+//
+//
+// const defaultAvatar = 'https://avatars3.githubusercontent.com/u1001';
+// userAvatarElem.src = defaultAvatar;
+//
+// //2.
+// const fetchUserData = userName => {
+//     return fetch(`https://api.github.com/users/${userName}`)
+//         .then(response => response.json());
+// }
+//
+// //3.
+// const renderUserData = userData => {
+//     const {avatar_url, name, location} = userData;
+//     userAvatarElem.src = avatar_url;
+//     userNameElem.textContent = name;
+//     userLocationElem.textContent = location ? `from ${location}` : '';
+// };
+//
+// //1-a.
+// const onSearchUser = () => {
+//     const userName = userNameInputElem.value;
+//     fetchUserData(userName)                           //
+//         .then(userData => renderUserData(userData)); //
+// };
+// //1.
+// showUserBtnElem.addEventListener('click', onSearchUser);
 
 
