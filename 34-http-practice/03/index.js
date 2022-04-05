4f3d06adbd46f5fb4013000587d0c966460d90fa
vsheva <valera.sheva@gmail.com>
2022-04-06 01:12:00 +0300

    'fix

--- 34-http-practice/03/index.js
+++ 34-http-practice/03/index.js
@@ -1,4 +1,22 @@
 'use strict'
+//algo
+
+//getting all DOM elements
+//http request
+// --- createUser (userData) => return fetch (method:POST)
+// --- button add listener ("click", onHandleClick) or on form add listener("submit"
+// --- onHandleClick (e) => create new Object
+//                       => подписываемся на fetch с помощью: createUser(newData).then
+//                       =>                     преобразуем js обьект в строку JSON с помощью JSON.stringify
+
+//validation
+//---allInputs add event Listener
+//---onValidHandler (проверка методом reportValidity)
+//--- removeAttribute
+//--- setAttribute
+
+
+
 const url = 'https://61eaff907ec58900177cdb49.mockapi.io/api/v1/registration';
 
 const emailInputElem = document.querySelector("#email");
@@ -6,10 +24,10 @@ const nameInputElem = document.querySelector("input[name='name']");
 const passwordInputElem = document.querySelector("input[name='password']");
 const registerButton = document.querySelector(".submit-button");
 const loginForm = document.querySelector(".login-form")
-const allInputsElems = document.querySelectorAll(".form-input")
+const allInputs = document.querySelectorAll(".form-input")
 
 //II
-const isValid = () => {
+const onValidHandler = () => {
     if (loginForm.reportValidity() === true) {
         registerButton.removeAttribute("disabled")
     } else {
@@ -17,9 +35,7 @@ const isValid = () => {
     }
 }
 
-allInputsElems.forEach(element => element.addEventListener("click", isValid))
-
-
+allInputs.forEach(element => element.addEventListener("input", onValidHandler))
 
 //1.
 function createUser(userData) {
@@ -34,7 +50,8 @@ function createUser(userData) {
 }
 
 //3.
-const onClickHandler = () => {
+const onClickHandler = (e) => {
+    e.preventDefault()
     let newData = {
         email: emailInputElem.value,
         name: nameInputElem.value,
@@ -45,7 +62,7 @@ const onClickHandler = () => {
         .then(data => alert(JSON.stringify(data)))
 }
 
-//2.
+//2. loginForm.addEventListener('submit', onClickHandler)
 registerButton.addEventListener('click', onClickHandler)
 
 
@@ -58,6 +75,271 @@ registerButton.addEventListener('click', onClickHandler)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//practice
+
+// const loginForm = document.querySelector(".login-form")
+// const emailInputElem = document.querySelector("#email")
+// const nameInputElem = document.querySelector("input[name='name']")
+// const passwordInputElem = document.querySelector("input[name='password']")
+// const registerButton = document.querySelector(".submit-button")
+// const allInputs = document.querySelectorAll(".form-input")
+//
+// //validation
+//
+// const isValid =() => {
+//     if(loginForm.reportValidity() ===true ) {
+//         registerButton.removeAttribute("disabled")
+//     } else {
+//         registerButton.setAttribute("disabled", "")
+//     }
+//
+//
+// }
+//
+// allInputs.forEach(element => element.addEventListener("input", isValid))
+//
+//
+//
+// const createUser = (userData) => {
+//     return fetch(url, {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(userData)
+//     })
+//         .then(response => response.json())
+// }
+//
+//
+// const onSubmitHandler = (e) => {
+//     e.preventDefault()
+//     const newUserData = {
+//         email: emailInputElem.value,
+//         name: nameInputElem.value,
+//         password: passwordInputElem.value,
+//     }
+//
+//     createUser(newUserData)
+//         .then(data=> alert(JSON.stringify(data)))
+// }
+//
+//
+//
+// registerButton.addEventListener("click", onSubmitHandler)
+//
 
 
 
