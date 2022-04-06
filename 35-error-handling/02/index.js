31ae580a83912cf7c1cb08a1194a50309353e960
vsheva <valera.sheva@gmail.com>
2022-04-06 18:05:05 +0300

    fix

--- 35-error-handling/02/index.js
+++ 35-error-handling/02/index.js
@@ -1,34 +1,79 @@
-//algo
-//  asyncCalculator(num)
-//  new Promise(resolve, reject)
-// .then
-//.then
-//.then
+const successRequest = Promise.resolve({ name: "Tom" });
 
- const asyncCalculator = (numbr) =>
-    new Promise((resolve) => {
-    setTimeout(() => {
-        console.log(`Initial value: ${numbr}`);
+successRequest
+    .then(function onSuccess1(data) {
+        // (1)
+        throw new Error("Error with data")
 
-        resolve(numbr);
-    }, 500);
+
+    })
+    .catch(function onError1(error) {    //<---1
+        console.error("onError1", error.message);
+    })
+
+
+const failRequest = Promise.reject(new Error("Something went wrong"));
+
+failRequest
+    .catch(function onError2(error) {
+        console.error("onError2", error.message);
+        // (2)
+        throw new Error("Server error")
+
+
+
+
+    })
+    .then(function onSuccess2(data) {
+        console.log("onSuccess2", data);
+    })
+    .catch(function onError3(error) {     //<---2
+        console.error("onError3", error.message);
+    });
+
+
+
+//Sample
+
+//1.
+
+// const succRequest = Promise.resolve({name: "Valera"})
+//
+// successRequest
+// .then(data=> {
+//     console.log(data)
+//     throw new Error("Unexpected Erroorrr!");
+// })
+// .catch(error=> {
+//     console.warn(error.message)
+// })
+
+//2.
+const failedRequest = Promise.reject(new Error("Faaaaaaail!"))
+
+failedRequest                                //игнорируется
+    .then(data=> {
+        console.log(data)
+
+
+    })
+    .catch(error=> {                             // <-----
+        console.warn(error.message)
+        // return Promise.reject("Another error")
+        // чтобы пробросить вниз (наружу) и не пошло в then
+         throw error;                           //<---
+    })
+    .then(data=> {
+        console.log(data)   //undefined
+
+    })
+    .catch(error=> {                             // <-----
+        console.warn(error.message)
+
+        throw error;
+    })
+
+//ловим ошибку снаружи сверху
+window.addEventListener("unhandledrejection", function(e) {
+    console.log(e.reason.message)
 })
-    .then(
-        (data) =>
-        new Promise((resolve) => {
-            setTimeout(() => {
-                let squared = data * data;
-                console.log(`Squared value: ${squared}`);
-                resolve(squared);
-            }, 500);
-        }))
-     .then((datas) =>{
-         let doubled = datas * 2;
-         console.log(`Doubled value: ${doubled}`)
-         return doubled;
-     });
-
-const result = asyncCalculator(5)
-
-
-
--- 35-error-handling/04/index.js
+++ 35-error-handling/04/index.js
@@ -1,29 +1,11 @@
-const serverResponsePromise = new Promise(resolve => {
-    const serverResponse = {
-        ok: true,
-        json() {
-            return Promise.resolve({
-                name: 'John',
-                age: 20,
-            });
-        },
-    };
-    resolve(serverResponse);
-});
+const parseUser = (jsonString) => {
+    try {
+        return JSON.parse(jsonString)
+    }
+    catch (err) {
+        return null
+    }
+}
 
-/*
- * допиши первый обработчик, чтобы во второй попал объект пользователя
- */
-
-serverResponsePromise
-    .then((response) => {
-        /* ...code here */
-        return response.json()
-    })
-    .then(result => {
-        console.log(result); // { name: 'John', age: 20 }
-    });
-
-console.log(
-    '!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!',
-);
+const parser = parseUser('{"firstName": "Valerii"}')
+console.log(parser)
