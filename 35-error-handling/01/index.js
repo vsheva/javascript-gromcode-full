ed7292056412d29580a2f85693bff2cc076ed25d
vsheva <valera.sheva@gmail.com>
2022-04-06 13:58:13 +0300

    fix

--- 35-error-handling/01/index.js
+++ 35-error-handling/01/index.js
@@ -1,27 +1,51 @@
-window.addEventListener("error", function onUnhandledError (err) {
-  console.error("error", err.message);
-});
+// window.addEventListener("error", function onUnhandledError (err) {
+//   console.error("error", err.message);
+// });
+//
+// let userParsingResult;
+//
+// try {
+//   const user = JSON.parse('{"firstName": "Valerii", "lastName":"Shevchenko"}');
+//   console.log("User data: ", user);
+//   userParsingResult = "success";
+// } catch(e) {
+//   userParsingResult = "error";
+// } finally {
+//   console.log(`User parsing finished with ${userParsingResult}`);
+// }
+//
+// let productParsingResult;
+//
+// try {
+//   const product = JSON.parse('{"firstName": "Valerii", "lastName":"Shevchenko');
+//   console.log("Product data: ", product);
+//   productParsingResult = "success";
+// } catch(e) {
+//   productParsingResult = "error";
+// } finally {
+//   console.log(`Product parsing finished with ${productParsingResult}`);
+// }
 
-let userParsingResult;
+
+window .addEventListener('error', function onUnhadledError(e){
+  console.log(e)
+})
+
+
 
 try {
-  const user = JSON.parse('{"firstName": "Valerii", "lastName":"Shevchenko"}');
-  console.log("User data: ", user);
-  userParsingResult = "success";
-} catch(e) {
-  userParsingResult = "error";
-} finally {
-  console.log(`User parsing finished with ${userParsingResult}`);
+
+  throw new ReferenceError();
+  const userData = JSON.parse('{"name":"Valera"')
+  console.log(userData)
+}
+catch (error) {
+  if(error instanceof SyntaxError) {
+    console.error("Handled")
+  } else {
+    throw error;
+  }
+  //console.error(error.name)
 }
 
-let productParsingResult;
-
-try {
-  const product = JSON.parse('{"firstName": "Valerii", "lastName":"Shevchenko');
-  console.log("Product data: ", product);
-  productParsingResult = "success";
-} catch(e) {
-  productParsingResult = "error";
-} finally {
-  console.log(`Product parsing finished with ${productParsingResult}`);
-}
+console.log("I'm alive")
