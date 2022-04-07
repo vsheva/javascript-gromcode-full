e035a8e73527ed89788688e192de2f2a2f401f92
vsheva <valera.sheva@gmail.com>
2022-04-08 01:03:57 +0300

    fix

---
+++ 36-promise-async-await/01/index.js
@@ -0,0 +1,33 @@
+// async function func() {
+//     const promise = new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve(17)
+//         }, 1500)
+//     })
+//   const result = await promise;
+//     console.log(result);
+//     console.log("Hello");
+// }
+//
+// const result = func()
+// console.log("Hi")
+
+export const fetchUser = async (userId) => {
+    try {
+        const response = await fetch(`https://api.github.com/users/${userId}`)
+        if (!response.ok) {
+            return null;
+        }
+        const userData = await response.json();
+        return userData; //object
+    } catch (err) {
+        throw new Error("Failed to get user data")
+    }
+};
+
+fetchUser("facebook")
+    .then(userData => console.log(userData))
+    .catch(err => alert(err.message))
+
+
+
