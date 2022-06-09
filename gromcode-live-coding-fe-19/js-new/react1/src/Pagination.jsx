5594dd6c3060a45ec90a1af6b8f02d962c228925
vsheva <valera.sheva@gmail.com>
2022-06-09 16:07:41 +0300

    fix

--- gromcode-live-coding-fe-19/js-new/react1/src/Pagination.jsx
+++ gromcode-live-coding-fe-19/js-new/react1/src/Pagination.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Pagination = ({ goPrev, goNext, itemsPerPage, totalItems, currentPage }) => {
-  const isPrevPageAvailable = currentPage <= 1;
-  const isNextPageAvailable = currentPage >= totalItems / itemsPerPage;
+  const isPrevPageAvailable = currentPage === 1;
+  const isNextPageAvailable = currentPage === Math.ceil(totalItems / itemsPerPage);
   console.log(isNextPageAvailable);
 
   return (
